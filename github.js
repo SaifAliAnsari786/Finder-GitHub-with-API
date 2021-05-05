f9908057877ae7a12bb4c9c8c787140a2232f60d
SaifAliAnsari786 <77094517+SaifAliAnsari786@users.noreply.github.com>
2021-05-05 17:55:23 +0545

    Add files via upload

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor(){
+        this.client_id = '9bf5385e00842779809e';
+        this.clinet_secret = ' 0aa7125b7dea9ac0fe2c1c243992f345f9fe70b9';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+    
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return{
+            profile,
+            repos
+        }
+    }
+}
